715a30d468f9064c05c16768c168450b159709d4
Vũ. Vy Quốc <vyquocvu@gmail.com>
2022-07-19 00:37:50 +0700

    remove airgram

--- src/airgram.tsx
+++ src/airgram.tsx
@@ -1,11 +1,5 @@
-import { Airgram } from 'airgram'
+import TdClient from 'tdweb/dist/tdweb';
 
 console.log("process.env.SOLID_APP_APP_ID", process.env.SOLID_APP_APP_ID);
 console.log("process.env.SOLID_APP_API_HASH", process.env.SOLID_APP_API_HASH);
 
-export const airgram = new Airgram({
-  apiId: Number(process.env.SOLID_APP_APP_ID),
-  apiHash: process.env.SOLID_APP_API_HASH,
-  command: process.env.TDLIB_COMMAND,
-  logVerbosityLevel: 2
-})
