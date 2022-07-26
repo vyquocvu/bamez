4f781b7c5c856cf0a31977cba601ed913facb622
Vũ. Vy Quốc <vyquocvu@gmail.com>
2022-07-26 22:39:46 +0700

    remove store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import type { Component } from 'solid-js';
-import { useStore } from '../store';
 
 const Home: Component = ()  => {
-  const { count } = useStore();
   return <>
     <h1 class='w-full'>
       Home
-      {count}
     </h1>
   </>
 }
